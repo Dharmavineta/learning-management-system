fdf924e8a2785f322400b9e8943059249961d6c4
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-01-25 19:35:14 +0530

    final

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
 export const Logo = () => {
-  return <Image height={130} width={130} alt="logo" src="/logo.png" />;
+  return (
+    <>
+      <div className="flex items-center gap-x-1">
+        <Image height={30} width={30} alt="logo" src="/logo.png" />
+        <h1 className="font-bold text-xl">LEARN</h1>
+      </div>
+    </>
+  );
 };
